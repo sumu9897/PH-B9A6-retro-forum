3264164671b3c3c02c6e988df50b09f2cf0a8053
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-05 11:46:11 +0600

    Search button part working

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const loadAllPosts = async() =>{
+const loadAllPosts = async(category) =>{
+
+    if(category){
+        console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`);
+    }else{
+        console.log(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+    }
+    
+
     // const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
     // const data = await response.json();
     // console.log(data.posts);
 }
 loadAllPosts();
 
-const handleSearchByCategory =() =>
+const handleSearchByCategory =() =>{
+    const searchText = document.getElementById("searchPosts").value;
+    loadAllPosts(searchText);
+    // console.log(searchText);
+}
