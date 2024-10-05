a4b4e96e77f0353399ad63692ba33cec6ef75a6c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-05 13:51:40 +0600

    Mark as Read Part Done

--- script.js
+++ script.js
@@ -89,8 +89,15 @@ const markAsRead =(description,view_count)=>{
                 </div>
     `
     markAsReadContainer.appendChild(div)
+    handleCount()
 }
+const handleCount =() =>{
+    const prevCount = document.getElementById("markAsReadCounter").innerText;
 
+    const convertedCount = parseInt(prevCount);
+    const sum = convertedCount + 1;
+    document.getElementById("markAsReadCounter").innerText = sum;
+}
 
 loadAllPosts();
 
