6ab00b072beac3b5c817b9f21a9285f3e1d50d95
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-05 13:16:50 +0600

    Card Add done

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const displayAllPost = (posts) =>{
     //console.log(posts)
     const postContainer = document.getElementById('post-container');
     posts.forEach(post => {
-        // console.log(post)
+         console.log(post)
         const div =document.createElement('div');
         div.innerHTML=`
 
@@ -35,28 +35,28 @@ const displayAllPost = (posts) =>{
  </div>
  <div class="space-y-4 w-full">
   <div class="flex gap-4 *:opacity-60">
-    <p> # category</p>
-    <p>Author : auther.name</p>
+    <p> # ${post.category}</p>
+    <p>Author : ${post.author.name}</p>
 
   </div>
-  <h3 class="text-2xl font-bold opacity-70">title</h3>
+  <h3 class="text-2xl font-bold opacity-70">${post.title}</h3>
   <p class="opacity-40">
-    description
+    ${post.description}
   </p>
   <hr class="border border-dashed border-gray-300">
   <div class="flex justify-between *:font-bold [&>*:not(:last-child)]:opacity-45">
     <div class="flex gap-4">
       <div class="space-x-2 flex items-center">
         <i class="fa fa-regular fa-comment-dots"></i>
-        <p>comment_count</p>
+        <p>${post.comment_count}</p>
       </div>
       <div class="space-x-2 flex items-center">
         <i class="fa fa-regular fa-eyes"></i>
-        <p>view_count</p>
+        <p>${post.view_count}</p>
       </div>
       <div class="space-x-2 flex items-center">
         <i class="fa fa-regular fa-clock"></i>
-        <p>posted_time Min</p>
+        <p>${post.posted_time} Min</p>
       </div>
 
     </div>
